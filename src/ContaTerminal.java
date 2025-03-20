afbb5b35da3414f6f1c34775abc596c05afd2f7e
AllanZter <santosgrupo1000@gmail.com>
2025-03-19 21:00:04 -0300

    Conta Terminal criada

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Scanner; // importação Classe Scanner
+
 public class ContaTerminal {
     public static void main(String[] args) {
+        // criando objeto Scanner
+        Scanner scanner = new Scanner(System.in);
+               
+        System.out.println("Olá, Vamos criar sua Conta Bancária. Por favor, digite seu nome:");
+        String NomeCliente = scanner.nextLine();
         
+        System.out.println("Por favor, digite o número da Agência !");
+        String Agencia = scanner.nextLine();
+        
+        System.out.println("Por favor, digite o número da conta:");
+        int Numero = scanner.nextInt();        
+
+        System.out.println("Por favor, digite seu saldo:");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + NomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é  "
+         + Agencia + ", conta  " + Numero + " e seu saldo R$ " + saldo + " já está disponível para saque.");      
+
     }
 }
